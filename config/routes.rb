0804f10a06b0f99e89e7b97291ea8b6da58c41b5
sk918 <skmtmt96918@gmail.com>
2021-02-08 00:03:58 +0900

    roomsテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=======
   devise_for :users
   get 'messages/index'
   root to: "messages#index"  
   resources :users, only: [:edit, :update]
->>>>>>> Stashed changes
 end
